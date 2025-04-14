61298c5841591b6fdc227e541f823f2307a1a239
yiming-liao <yimingliao.official@gmail.com>
2025-04-14 21:42:09 +0700

    update export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as FileValidator } from "./file-validator";
+export { FileValidationError } from "./error/file-validator-error";
 export { FileValidatorErrorCode } from "./error/error-code.types";
 export * from "./file-validator.types";
